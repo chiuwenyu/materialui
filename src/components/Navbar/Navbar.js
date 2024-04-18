ca4dd91cdfe7d33623eaaa305a39a3bdc06f72ed
WENYU CHIU <chiuwenyu@gmail.com>
2024-04-18 11:10:53 +0800

    Route dir implement

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,12 +7,14 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import { mainNavbarItems } from "./consts/navbarItem";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const drawerWidth = 220;
+  const navigate = useNavigate();
+  const { route } = useParams();
+
   return (
     <div>
       <Drawer
@@ -22,6 +24,8 @@ const Navbar = () => {
           "& .MuiDrawer-paper": {
             width: drawerWidth,
             boxSizing: "border-box",
+            bgcolor: "#101f33",
+            color: "rgba(255, 255, 255, 0.7)",
           },
         }}
         variant="permanent"
@@ -30,26 +34,17 @@ const Navbar = () => {
         <Toolbar />
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          {mainNavbarItems.map((item, index) => (
+            <ListItem
+              key={item.id}
+              disablePadding
+              onClick={() => navigate(item.route)}
+            >
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <ListItemIcon sx={{ color: "rgba(255,255,255,0.7)" }}>
+                  {item.icon}
                 </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={item.label} />
               </ListItemButton>
             </ListItem>
           ))}
