e73e0f54825f221a95dd53f4cda1af38e8a04d48
WENYU CHIU <chiuwenyu@gmail.com>
2024-04-18 23:11:01 +0800

    Overriding styles using SX props

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,8 +1,39 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import CommonButton from "../../components/common/CommonButton/CommonButton";
 
 const Authentication = () => {
-  return <Grid xs={8}>This is authentication page</Grid>;
+  const buttonStyles = {
+    fontSize: "0.875rem",
+    fontWeight: 600,
+    textTransform: "capitalize",
+    borderRadius: 2,
+    "&.MuiButton-contained": {
+      backgroundColor: "#009be5",
+      "&:hover": {
+        backgroundColor: "#006db3",
+      },
+    },
+    "&.MuiButton-outlined": {
+      color: "#fff",
+      borderColor: "#fff",
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
+  };
+
+  return (
+    <Grid item xs={8}>
+      This is authentication page.
+      <CommonButton sx={buttonStyles} variant="contained">
+        Add user
+      </CommonButton>
+      <CommonButton sx={buttonStyles} variant="outlined">
+        Add user
+      </CommonButton>
+    </Grid>
+  );
 };
 
 export default Authentication;
